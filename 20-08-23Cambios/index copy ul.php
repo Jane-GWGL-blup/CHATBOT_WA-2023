0761a077bb730f60c0360205f599037c90c7bceb
Nicole <janenieggcs@gmail.com>
2023-08-20 15:39:25 -0500

    Funciona la lista de botones y los reply buttons

--- 20-08-23Cambios/index copy ul.php
+++ 20-08-23Cambios/index copy ul.php
@@ -1,8 +1,8 @@
 <?php
 //DESHABILITAMOS EL MOSTRAR ERRORES
-/*ini_set('display_errors', 0);
+ini_set('display_errors', 0);
 ini_set('display_startup_errors', 0);
-error_reporting(-1);*/
+error_reporting(-1);
 
 require 'vendor/autoload.php';
 
